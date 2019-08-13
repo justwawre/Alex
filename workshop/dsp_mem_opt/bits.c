592c90f3a35fcf704bab019bdf8e3b8c6960a00a
Alex Xu <xuyc@sina.com>
2019-08-13 17:13:28 +0800

    modified:   dsp_mem_opt/bits.c

--- workshop/dsp_mem_opt/bits.c
+++ workshop/dsp_mem_opt/bits.c
@@ -3,36 +3,36 @@ gcc -masm=intel -S
 
 main:
 .LFB0:
-	.cfi_startproc
-	push	rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	mov	rbp, rsp
-	.cfi_def_cfa_register 6
-	mov	DWORD PTR -8[rbp], 0
+.cfi_startproc
+push	rbp
+.cfi_def_cfa_offset 16
+.cfi_offset 6, -16
+mov	rbp, rsp
+.cfi_def_cfa_register 6
+mov	DWORD PTR -8[rbp], 0
 
-    //init the struct
-	movzx	eax, BYTE PTR -8[rbp]
-	or	eax, 3
-	mov	BYTE PTR -8[rbp], al
+//init the struct
+movzx	eax, BYTE PTR -8[rbp]
+or	eax, 3
+mov	BYTE PTR -8[rbp], al
 
-    //b.mid  = 33;
-	movzx	eax, WORD PTR -8[rbp]
-    and	ax, -4093  //0XFF03
-	or	al, -124   //0x84=132=33*4
-	mov	WORD PTR -8[rbp], ax
+//b.mid  = 33;
+movzx	eax, WORD PTR -8[rbp]
+and	ax, -4093  //0XFF03
+or	al, -124   //0x84=132=33*4
+mov	WORD PTR -8[rbp], ax
 
-    //xxx = b.mid;
-    movzx	eax, WORD PTR -8[rbp]
-	shr	ax, 2
-	and	ax, 1023
-    movzx	eax, ax
-	mov	DWORD PTR -4[rbp], eax
+//xxx = b.mid;
+movzx	eax, WORD PTR -8[rbp]
+shr	ax, 2
+and	ax, 1023
+movzx	eax, ax
+mov	DWORD PTR -4[rbp], eax
 
-	mov	eax, 0
-	pop	rbp
-	.cfi_def_cfa 7, 8
-	ret
+mov	eax, 0
+pop	rbp
+.cfi_def_cfa 7, 8
+ret
 */
 struct bits
 {
