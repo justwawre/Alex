1de7a0323933cf1a3268aacebf0837bbfb0b6649
Alex Xu <xuyc@sina.com>
2020-05-06 21:36:54 +0800

    modified:   IO.cpp

--- C++/IO.cpp
+++ C++/IO.cpp
@@ -14,29 +14,12 @@ g++ -std=c++1z -g IO.cpp
 #include <sstream>
 #include <iomanip>
 
-#include "../toolkit.h"
+#include "toolkit.h"
 using namespace std;
 
 vector<int> read_ints(istream &is)
 {
-    FUNC_HEAD();
-    cout << "please input integer to a vector" << endl;
-    vector<int> res;
-    int i;
-    while (is >> i)
-        res.push_back(i);
-    if (is.eof())
-    {
-        is.clear();
-    }
-
-    return res;
-}
-
-vector<int> read_ints_2(istream &is)
-{
-    FUNC_HEAD();
-    cout << "please input integer to a vector" << endl;
+    cout << "please input integer: 1 ,2 ,{4 5}" << endl;
     vector<int> res;
     int i;
 
@@ -45,11 +28,9 @@ vector<int> read_ints_2(istream &is)
         for (int i; is >> i;)
         {
             res.push_back(i);
-            cout << "got\t" << i << endl;
         }
         if (is.eof())
         {
-            cout << "got eof" << endl;
             is.clear();
             break; // .. all is well we reached the end-of-file ...
         }
@@ -64,32 +45,25 @@ vector<int> read_ints_2(istream &is)
                 case ',':
                 case '{':
                 case '}':
-                    cout << "got " << ch << endl;
                     continue;
                     break;
-
                 default:
                     is.setstate(ios_base::failbit); // add fail() to cin’s state
                 }
             }
         }
-        // ...
     }
     return res;
 }
 
-void input_simple()
+void input_nested_test()
 {
     FUNC_HEAD();
     vector<int> v = read_ints(cin);
-    cout << "Got " << v.size() << " elements" << endl;
-}
-
-void input_nested()
-{
-    FUNC_HEAD();
-    vector<int> v = read_ints_2(cin);
-    cout << "Got " << v.size() << " elements" << endl;
+    cout << "got  ";
+    for (auto i : v)
+        cout << i << " ";
+    cout << endl;
 }
 
 struct Entry
@@ -104,7 +78,6 @@ ostream &operator<<(ostream &os, const Entry &e)
 }
 
 istream &operator>>(istream &is, Entry &e)
-// read { "name" , number } pair. Note: formatted with { " " , and }
 {
     char c, c2;
     if (is >> c && c == '{' && is >> c2 && c2 == '"')
@@ -126,17 +99,17 @@ istream &operator>>(istream &is, Entry &e)
     return is;
 }
 
-void usser_defined_type()
+void custom_type_test()
 {
     FUNC_HEAD();
     Entry e;
-    cout << "please input entry in format { \"name\" , number }" << endl;
+    cout << "please input 1 item as { \"name\" , number }" << endl;
     cin >> e;
     cout << "we got:" << endl;
     cout << e << endl;
 }
 
-void output_format()
+void output_format_test()
 {
     FUNC_HEAD();
     constexpr double d = 123.456;
@@ -214,7 +187,7 @@ void output_format()
     print_break();
 }
 
-void test_stringstream()
+void stringstream_test()
 {
     FUNC_HEAD();
     ostringstream oss;
@@ -223,16 +196,16 @@ void test_stringstream()
     cout << oss.str() << '\n';
 }
 
+/*
+在windows平台，stdin输入流的EOF标志是 ctrl + z。
+在unix/linux平台，stdin输入流的EOF标志是 ctrl + d
+那么WSL下呢？
+*/
 main()
 {
-    /*
-    now 1 ctrl-d will terminate the followed input, so have to comment one 
-*/
-    //input_simple();
-    //input_nested();
-
-    //usser_defined_type();
-
-    output_format();
-    test_stringstream();
+    input_nested_test();
+    custom_type_test();
+    output_format_test();
+    stringstream_test();
+    return 0;
 }
